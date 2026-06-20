5ee2e5eda46afb9488a48f209fa1b169d697a0cb
JP Etcheber <jetcheber@gmail.com>
2026-06-20 11:19:17 -0700

    fix: add CORS allowance for klecks

--- main.go
+++ main.go
@@ -109,6 +109,21 @@ func main() {
 
 	r := gin.Default()
 
+	// Enable CORS middleware
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
+
 	// Set trusted proxies to nil to resolve the security warning.
 	r.SetTrustedProxies(nil)
 
